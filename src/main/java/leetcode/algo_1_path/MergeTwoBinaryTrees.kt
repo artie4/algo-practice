d5fd7e8e528a67dd0e12c518ac650915bca921c2
arthur <gaartur4@gmail.com>
2022-04-11 07:31:24 +0300

    add merge of two bin trees

---
+++ src/main/java/leetcode/algo_1_path/MergeTwoBinaryTrees.kt
@@ -0,0 +1,30 @@
+package leetcode.algo_1_path;
+
+
+class MergeTwoBinaryTrees {}
+
+// run with jvm option -ea
+fun main() {
+    val tree1 = TreeNode(1)
+    val tree2 = TreeNode(3).apply { left = TreeNode(2); right = TreeNode(4) }
+    val mergedTree = mergeTrees(tree1, tree2)
+    assert(mergedTree!!.`val` == 4)
+    assert(mergedTree.left!!.`val` == 2)
+    assert(mergedTree.right!!.`val` == 4)
+}
+
+fun mergeTrees(root1: TreeNode?, root2: TreeNode?): TreeNode? {
+    if (root1 == null) return root2
+    if (root2 == null) return root1
+
+    root1.`val` += root2.`val`
+
+    root1.left = mergeTrees(root1.left, root2.left)
+    root1.right = mergeTrees(root1.right, root2.right)
+    return root1
+}
+
+class TreeNode(var `val`: Int) {
+    var left: TreeNode? = null
+    var right: TreeNode? = null
+}
