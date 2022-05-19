0a2cb2914f523b0fcc8c983303df76f1d3a7b136
arthur <gaartur4@gmail.com>
2022-05-19 23:44:41 +0300

    add parentheses validation

---
+++ src/main/java/leetcode/ValidParentheses.kt
@@ -0,0 +1,30 @@
+package leetcode
+
+import java.util.*
+
+class ValidParentheses {
+    fun isValid(s: String): Boolean {
+        val stack = Stack<Char>()
+        for (ch in s) {
+            when(ch) {
+                '(' -> stack.push(')')
+                '{' -> stack.push('}')
+                '[' -> stack.push(']')
+                else -> {
+                    if (stack.empty() || stack.pop() != ch) return false
+                }
+            }
+        }
+        if (!stack.empty()) return false
+        return true
+    }
+}
+
+fun main() {
+    val inst = ValidParentheses()
+
+    assert(inst.isValid("{}()[]"))
+    assert(inst.isValid("{()}[{}]"))
+    assert(!inst.isValid("(()}[{}]"))
+
+}
