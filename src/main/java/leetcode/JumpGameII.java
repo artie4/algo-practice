fdcbcf90a333dd46854aa86f4c145623224fc0c9
arthur <gaartur4@gmail.com>
2022-08-06 16:15:48 +0300

    add jump game 2

---
+++ src/main/java/leetcode/JumpGameII.java
@@ -0,0 +1,35 @@
+package leetcode;
+
+public class JumpGameII {
+    public int jump(int[] nums) {
+        if (nums == null || nums.length < 2) {
+            return 0;
+        }
+
+        int jumps = 0;
+        int currentLenght = nums[0];
+        int currMaxLength = nums[0];
+
+        for (int i = 1; i < nums.length - 1; ++i) {
+            currentLenght--;
+            currMaxLength--;
+            if (nums[i] > currMaxLength) {
+                currMaxLength = nums[i];
+            }
+            if (currentLenght == 0) {
+                jumps++;
+                currentLenght = currMaxLength;
+            }
+        }
+        return jumps + 1;
+    }
+
+    public static void main(String[] args) {
+
+        JumpGameII jumpGameII = new JumpGameII();
+        assert (jumpGameII.jump(new int[]{1,2}) == 1);
+        assert (jumpGameII.jump(new int[]{2,1}) == 1);
+        assert (jumpGameII.jump(new int[]{2,3,1,1,4}) == 2);
+        assert (jumpGameII.jump(new int[]{1,1,1,1}) == 3);
+    }
+}
