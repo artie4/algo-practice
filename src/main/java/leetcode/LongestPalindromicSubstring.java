4215431cac5d86834ec7aa3b873531a40e1180c9
arthur <gaartur4@gmail.com>
2022-08-24 21:28:02 +0300

    edit longest palindromic substring

--- src/main/java/leetcode/LongestPalindromicSubstring.java
+++ src/main/java/leetcode/LongestPalindromicSubstring.java
@@ -4,9 +4,10 @@ public class LongestPalindromicSubstring {
     public String longestPalindrome(String s) {
         if (s == null || s.length() < 1) return "";
         int start = 0, end = 0;
-        for (int i = 0; i < s.length(); i++) {
-            int len1 = expandAroundCenter(s, i, i);
-            int len2 = expandAroundCenter(s, i, i + 1);
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            int len1 = expandAroundCenter(chars, i, i);
+            int len2 = expandAroundCenter(chars, i, i + 1);
             int len = Math.max(len1, len2);
             if (len > end - start) {
                 start = i - (len - 1) / 2;
@@ -16,18 +17,19 @@ public class LongestPalindromicSubstring {
         return s.substring(start, end + 1);
     }
 
-    private int expandAroundCenter(String s, int left, int right) {
-        int L = left, R = right;
-        while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
-            L--;
-            R++;
+    private int expandAroundCenter(char[] chars, int left, int right) {
+        int l = left, r = right;
+        while (l >= 0 && r < chars.length && chars[l] == chars[r]) {
+            l--;
+            r++;
         }
-        return R - L - 1;
+        return r - l - 1;
     }
 
     public static void main(String[] args) {
         LongestPalindromicSubstring instance = new LongestPalindromicSubstring();
         assert(instance.longestPalindrome("cabbaba").equals("abba"));
+        assert(instance.longestPalindrome("abde").equals("e"));
     }
 }
 
